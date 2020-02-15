de89cc0e27e769a4939c10d8cf976b827c4679f6
Jihwan Kim <noneobj@gmail.com>
2020-02-16 08:36:16 +0900

    Change style of Top : add max-width

--- src/components/top/index.jsx
+++ src/components/top/index.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import { GitHubIcon } from '../social-share/github-icon'
-
+import { rhythm } from '../../utils/typography'
 import './index.scss'
 
 export const Top = ({ title, location, rootPath }) => {
   const isRoot = location.pathname === rootPath
   return (
     <div className="top">
-      {!isRoot && (
-        <Link to={`/`} className="link">
-          {title}
-        </Link>
-      )}
-      <GitHubIcon />
+      <div style={{
+        marginLeft: `auto`,
+        marginRight: `auto`,
+        maxWidth: rhythm(30),
+      }}>
+        {!isRoot && (
+          <Link to={`/`} className="link">
+            {title}
+          </Link>
+        )}
+        <GitHubIcon />
+      </div>
     </div>
   )
 }
