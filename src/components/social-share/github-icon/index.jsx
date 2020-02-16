4478bc705637daa14c44b6ad15c652d669d43735
Jihwan Kim <noneobj@gmail.com>
2020-02-16 10:25:41 +0900

    Change top github link to redirect to blog repo

--- src/components/social-share/github-icon/index.jsx
+++ src/components/social-share/github-icon/index.jsx
@@ -5,7 +5,7 @@ import './index.scss'
 export const GitHubIcon = () => {
   return (
     <a
-      href="https://github.com/veatoriche"
+      href="https://github.com/veatoriche/veatoriche-blog"
       className="github"
       aria-label="GitHub"
     >
